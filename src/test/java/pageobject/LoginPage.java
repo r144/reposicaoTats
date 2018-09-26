59384d54dd9183d1feb9ea841f1dbe4b72433c98
r144 <renatoc@alunos.utfpr.edu.br>
2018-09-25 21:39:58 -0300

    adicionando private em LoginPage

--- src/test/java/pageobject/LoginPage.java
+++ src/test/java/pageobject/LoginPage.java
@@ -7,20 +7,46 @@ import org.openqa.selenium.support.FindBy;
 public class LoginPage extends RedmineBasePage{
 
     @FindBy(id = "username")
-    WebElement inputUserName;
+    private WebElement inputUserName;
 
     @FindBy(id = "password")
-    WebElement inputPassword;
+    private WebElement inputPassword;
 
     @FindBy(name = "login")
-    WebElement buttonLogin;
+    private WebElement buttonLogin;
 
     @FindBy(linkText = "Perdi minha senha")
-    WebElement linkLostPassowrd;
+    private WebElement linkLostPassowrd;
 
     public LoginPage(WebDriver driver) {
         super(driver);
     }
 
+    public WebElement getInputUserName() {
+        return inputUserName;
+    }
+
+    public WebElement getInputPassword() {
+        return inputPassword;
+    }
+
+    public WebElement getButtonLogin() {
+        return buttonLogin;
+    }
+
+    public WebElement getLinkLostPassowrd() {
+        return linkLostPassowrd;
+    }
+
+    public void DoLogin(String user, String password){
+        WebElement inputUser = getInputUserName();
+        WebElement inputPass = getInputPassword();
+        WebElement buttonLogin = getButtonLogin();
+
+        inputUser.sendKeys(user);
+        inputPass.sendKeys(password);
+
+        buttonLogin.click();
+    }
 
 }
