8c3e8e88cea08f905a216580430e0bbe13e2f448
r144 <renatoc@alunos.utfpr.edu.br>
2018-09-25 23:00:13 -0300

    Refatorando as funções de register e de login para preencher os campos

--- src/test/java/redmineTests/TesteExemplo01.java
+++ src/test/java/redmineTests/TesteExemplo01.java
@@ -1,10 +1,20 @@
-/*
+package redmineTests;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,13 +22,28 @@ import static org.junit.Assert.*;
  * @author viniciusbs
  */
 public class TesteExemplo01 {
-    
-    public TesteExemplo01() {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public static void setUpClass() { WebDriverManager.chromedriver().setup(); }
+
+    @Before
+    public void setUp() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //chromeOptions.addArguments("headless");
+        chromeOptions.addArguments("window-size=1200x600");
+        chromeOptions.addArguments("start-maximized");
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @After
+    public void tearDown() { driver.close(); }
+
+
+    @Test
+    public void Test() {}
 }
+
